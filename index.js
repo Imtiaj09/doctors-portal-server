f4df63704c8314a4ffda67b67d1748418ec6cd73
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-19 18:33:20 +0600

    create a POST API for all doctors

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ async function run() {
     const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
     const bookingCollection = client.db('doctorsPortal').collection('bookings')
     const usersCollection = client.db('doctorsPortal').collection('users')
+    const doctorsCollection = client.db('doctorsPortal').collection('doctors')
 
 
     //Use Aggregate to query multiple collection and then merge data
@@ -175,7 +176,20 @@ async function run() {
       }
       const result = await usersCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
+    });
+
+    app.get('/doctors', async (req, res) => {
+      const query = {};
+      const doctors = await doctorsCollection.find(query).toArray();
+      res.send(doctors)
     })
+
+    //API for doctor list
+    app.post('/doctors', async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorsCollection.insertOne(doctor);
+      res.send(result);
+    });
 
   }
   finally {
